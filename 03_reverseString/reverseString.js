0698163f8dd53dd7cc9fe590d991b161182d6393
momintlh <77355191+momintlh@users.noreply.github.com>
2023-09-29 23:03:22 +0500

    reverse string cleared!

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (word) {
+  let newString = "";
 
+  for (let i = word.length - 1; i >= 0; i--) {
+    newString += word[i];
+  }
+  return newString;
+
+  // or return word.split("").reverse().join("");
 };
 
 // Do not edit below this line
