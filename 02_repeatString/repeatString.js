3c894b76876ef211a38aae06af55b6ff6da54102
momintlh <77355191+momintlh@users.noreply.github.com>
2023-09-29 22:44:50 +0500

    Repeating string cleared!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function (repeatingString, noOfTimes) {
+  let newString = "";
 
+  if (noOfTimes < 0) return "ERROR";
+
+  for (let index = 0; index < noOfTimes; index++) {
+    newString += repeatingString;
+  }
+  return newString;
 };
 
 // Do not edit below this line
