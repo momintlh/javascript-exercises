fc0a2951138d6cef64bd9067379cbb104a5e7bdc
momintlh <77355191+momintlh@users.noreply.github.com>
2023-09-30 19:41:46 +0500

    sumAll cleared!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function () {};
+const sumAll = function (start, end) {
+  let sum = 0;
+
+  if (start > end) {
+    const temp = start;
+    start = end;
+    end = temp;
+  }
+
+  if (typeof start != "number" || typeof end != "number" || start < 0) {
+    return "ERROR";
+  } else {
+    for (let i = start; i <= end; i++) {
+      sum += i;
+    }
+    return sum;
+  }
+};
 
 // Do not edit below this line
 module.exports = sumAll;
