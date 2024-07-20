f045dd68a87c4abfe3c8f9fb478740dd719ff695
momintlh <77355191+momintlh@users.noreply.github.com>
2024-07-21 01:40:34 +0500

    feat: map task

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,15 @@
-const getTheTitles = function() {
+const getTheTitles = function (books) {
+  /* const a = [];
 
+  for (let i = 0; i < books.length; i++) {
+    a.push(books[i].title);
+  }
+  return a;
+
+  */
+
+  // using array.map():
+  return books.map((book) => book.title);
 };
 
 // Do not edit below this line
