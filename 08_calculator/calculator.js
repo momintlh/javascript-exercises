80e903d257efb75c375a637ba51dfa8ed0e052be
momintlh <77355191+momintlh@users.noreply.github.com>
2024-07-21 01:21:02 +0500

    feat: solved object exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((total, item) => total + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((total, item) => total * item);
 };
 
-const power = function() {
-	
+const power = function (x, y) {
+  return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function (x) {
+  if (x === 0) return 1;
+
+  let answer = 1;
+
+  for (let i = 2; i <= x; i++) {
+    answer *= i;
+  }
+
+  return answer;
 };
 
 // Do not edit below this line
@@ -29,5 +37,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
